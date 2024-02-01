d8de243ce77f5b1a2d7623af2db8f2b3379d8f6a
Gauri Maheshwari <100439627+gauriimaheshwarii@users.noreply.github.com>
2024-02-01 08:12:57 +0000

    modify files

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -10,42 +10,34 @@ import React from "react";
  * About background image
  *
  * Below is a sample image. Upload the image of your choice into the "images"
- * directory and import here for use. Then, set imageAltText to string that 
+ * directory and import here for use. Then, set imageAltText to string that
  * represents what you see in that image.
  *
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "../images/bg.jpg";
 
-const imageAltText = "purple and blue abstract background";
+const imageAltText = "f1 abstract background";
 
 /**
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a full stack developer and a DevOps enthusiast studying at Indira Gandhi Delhi Technical University for Women.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
-const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
-];
+const skillsList = ["Full stack development", "DevOps", "Mechanical Engineering", "Formula 1"];
 
 /**
  * Use this to give more information about what you are passionate about,
  * how you best work, or even a quote. This will help someone learn more
  * about you on a professional level.
  */
-const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+const detailOrQuote = "Fear is temporary, regret is forever.";
 
 const About = () => {
   return (
